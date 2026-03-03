e512cdc14cb86e7b23eaa5176405c37c3dbf0fee
あなたの名前 <kusoowari@gmail.com>
2026-03-03 14:42:17 +0900

    fix: setup proper provider structure

---
+++ src/hooks/GlobalContextProvider.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { WebSocketProvider } from './websocket';
+import { SystemContextProvider } from './system';
+import { ErrorOverlayProvider } from './error-overlay';
+import { PlayerIdentityProvider } from './player-identity';
+import { MatchingProvider } from './matching';
+
+interface Props {
+  children: ReactNode;
+}
+
+export const GlobalContextProvider = ({ children }: Props) => {
+  return (
+    <ErrorOverlayProvider>
+      <WebSocketProvider>
+        <PlayerIdentityProvider>
+          <SystemContextProvider>
+            <MatchingProvider>{children}</MatchingProvider>
+          </SystemContextProvider>
+        </PlayerIdentityProvider>
+      </WebSocketProvider>
+    </ErrorOverlayProvider>
+  );
+};
