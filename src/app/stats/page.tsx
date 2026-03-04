cb4ff2605853402ddff925ee546647d14a8904a7
あなたの名前 <kusoowari@gmail.com>
2026-03-04 16:24:25 +0900

    final: force disable everything

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,9 +1,5 @@
 export const dynamic = 'force-dynamic';
 
-export default async function Page() {
-  return (
-    <main>
-      <h1>Success</h1>
-    </main>
-  );
+export default function Page() {
+  return "OK";
 }
