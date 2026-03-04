5ab23669a5ec2e8f27f41535d933045a2f9e5669
あなたの名前 <kusoowari@gmail.com>
2026-03-04 16:45:11 +0900

    fix: nuclear option - downgrade to next 14

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: 'standalone'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
